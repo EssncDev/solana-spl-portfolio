89fa8519238ba9d33af0547cf1888779027cab97
EssncDev <138819662+EssncDev@users.noreply.github.com>
2024-02-09 10:19:32 +0100

    PR #2
    Added Solana Price Display;
    Small Infra Update

--- services/scraper/jup.js
+++ services/scraper/jup.js
@@ -1,13 +1,13 @@
 async function fetchJupData(currency) {
     try {
       const response = await fetch(`https://price.jup.ag/v4/price?ids=${currency}`);
-      if(response.ok){
+      if (response.ok) {
         const data = await response.json();
         return(data.data[currency]);
-      }else{
+      } else {
         return(false)
       }
-    }catch(error){
+    } catch(error) {
       console.error('Error fetching Jup data:', error);
     }
   }
