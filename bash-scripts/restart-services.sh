26f9c8b9cffce10196241aac3553d47e4f09672a
EssncDev <138819662+EssncDev@users.noreply.github.com>
2024-02-10 11:53:41 +0100

    bash update PR#3

--- bash-scripts/restart-services.sh
+++ bash-scripts/restart-services.sh
@@ -3,7 +3,8 @@
 # Start PM2 services
 echo "Restarting PM2 services..."
 
-pm2 restart all 
+pm2 restart ./services/redis/token-scraper.js
+pm2 restart ./services/api/api.js
 npm start 
 
 echo "PM2 services restared successfully."
